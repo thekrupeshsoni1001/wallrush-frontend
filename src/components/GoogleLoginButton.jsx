65e10dac397700275efd0e38371d7e880151c27b
Krupesh <krupeshsoni57@gmail.com>
2026-01-29 17:16:56 +0530

    Fix Google login API URL

--- src/components/GoogleLoginButton.jsx
+++ src/components/GoogleLoginButton.jsx
@@ -2,19 +2,21 @@ import { GoogleLogin } from "@react-oauth/google";
 import { jwtDecode } from "jwt-decode";
 import axios from "axios";
 
+const API = axios.create({
+    baseURL: process.env.REACT_APP_API_URL,
+    withCredentials: true,
+});
+
 function GoogleLoginButton({ onLogin }) {
     const handleSuccess = async (response) => {
         try {
             const decoded = jwtDecode(response.credential);
 
-            const res = await axios.post(
-                "http://localhost:5000/api/auth/google-login",
-                {
-                    name: decoded.name,
-                    email: decoded.email,
-                    picture: decoded.picture,
-                }
-            );
+            const res = await API.post("/auth/google-login", {
+                name: decoded.name,
+                email: decoded.email,
+                picture: decoded.picture,
+            });
 
             const authData = {
                 token: res.data.token,
